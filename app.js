9a020c7cc7b75c44c5c2375b2911a22354e835bb
talhaali0123 <50287643+talhaali0123@users.noreply.github.com>
2024-10-04 21:09:03 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,128 @@
+// FUNCTION
+
+
+// function greet(){
+//     console.log("Function call")
+// };
+
+// // greet (); // function calling
+
+// greet();
+// greet();
+// greet();
+
+// function sum() {
+//     var a = 10;
+//     b = 20;
+//     console.log(a+b);
+
+// }
+// sum();
+
+
+// function welcome(greet) {  // function parameter se value leta hai
+//    // var greet = "Hello JS Function";
+//     console.log(greet);
+// }
+
+// welcome("Hello JS Function"); // function ko value dena argument // 
+// welcome(100);
+// welcome(true);
+
+
+// var num1= +prompt("Enter first no");
+// var num2= +prompt("Enter second no");
+
+
+// function add(num1,num2){
+//     console.log(num1+num2);
+// }
+// add(num1,num2);
+
+
+
+// function add(num1,num2){
+//     console.log(num1+ " " + num2);
+// }
+// add(+prompt("Enter first no"),+prompt("Enter second no"));
+// add(prompt("Enter first name") , prompt("Enter last name"));
+
+
+// function message(name,age,city){
+//     console.log(`Hello My name is ${name}. I am ${age} years old. I lived in ${city}`)
+// }
+// message("Talha", 22,"Karachi");
+
+
+// Table Program
+
+// function multiplicationTable(table,range){
+//     for(var i=1;i<range;i++){
+//      document.write(`${table} x ${i} = ${table * i} <br`)   
+//     }
+// }
+
+// multiplicationTable(+prompt("Enter any number"),+prompt("Enter range"));
+
+// function sum(a,b,c){
+//     console.log(a+b+c);
+// }
+
+// sum(50);
+
+
+// function multi(a,b){
+//     var res = a * b;
+//     return res;
+// }
+
+// var get = multi(5,6);
+// console.log(get + 10);
+
+
+// function greet(message){
+//     return message;
+// }
+// var result = greet("Function call");
+
+// console.log(result);
+
+
+// Calculator program
+
+// function calc(num1,num2,operator){
+//     if( operator === "+"){
+//         return num1+num2;
+//     }
+
+//     else if(operator === "-"){
+//         return num1-num2;
+//     }
+//     else if(operator === "*"){
+//         return num1*num2;
+//     }
+//     else if(operator === "/"){
+//         return num1/num2;
+//     }
+//     else if(operator === "%"){
+//         return num1%num2;
+//     }
+//     else{
+//         return "Invalid operator"
+//     }
+// }
+
+// var result = calc(
+// +prompt("Enter First number"),
+// +prompt("Enter second number"),
+// prompt("Enter operator")
+// );
+
+// console.log(result);
+
+
+//jo variable function ke ander wo local
+
+// jo varibale function ke bahar ho wo global variable hoga
+
+
